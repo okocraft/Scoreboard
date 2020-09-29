27b68183abedda544e870d51b082e74be9751beb
Siroshun09 <siro.shun8@gmail.com>
2020-09-29 23:50:17 +0900

    [Change] 各数値を定数として定義する

--- src/main/java/net/okocraft/scoreboard/util/Colorizer.java
+++ src/main/java/net/okocraft/scoreboard/util/Colorizer.java
@@ -7,6 +7,8 @@ public final class Colorizer {
     private static final char COLOR_CHAR = '§';
     private static final char HEX_MARK = '#';
     private static final char X = 'x';
+    private static final int HEX_COLOR_CODE_LENGTH = 7;
+    private static final int HEXADECIMAL = 16;
     private static final String COLOR_CODES = "0123456789AaBbCcDdEeFfKkLlMmNnOoRr";
 
     private Colorizer() {
@@ -36,23 +38,25 @@ public final class Colorizer {
                 continue;
             }
 
-            if (b[i + 1] == HEX_MARK) {
-                if (i + 7 < b.length) {
+            int next = i + 1;
+
+            if (b[next] == HEX_MARK) {
+                if (i + HEX_COLOR_CODE_LENGTH < b.length) {
                     try {
-                        addMcColor(str.substring(i + 1, i + 8), builder);
-                        i += 7;
+                        addMcColor(str.substring(next, next + HEX_COLOR_CODE_LENGTH), builder);
+                        i += HEX_COLOR_CODE_LENGTH;
                         continue;
                     } catch (IllegalArgumentException ignored) {
                     }
                 }
 
-                builder.append(b[i]).append(b[i + 1]);
+                builder.append(b[i]).append(b[next]);
                 i++;
                 continue;
             }
 
-            if (-1 < COLOR_CODES.indexOf(b[i + 1])) {
-                builder.append(COLOR_CHAR).append(Character.toLowerCase(b[i + 1]));
+            if (-1 < COLOR_CODES.indexOf(b[next])) {
+                builder.append(COLOR_CHAR).append(Character.toLowerCase(b[next]));
                 i++;
             } else {
                 builder.append(b[i]);
@@ -65,7 +69,7 @@ public final class Colorizer {
     private static void addMcColor(String hex, StringBuilder builder) throws IllegalArgumentException {
         char[] array = hex.toCharArray();
 
-        if (array.length != 7) {
+        if (array.length != HEX_COLOR_CODE_LENGTH) {
             throw new IllegalArgumentException("hex must be 7 characters.");
         }
 
@@ -74,7 +78,7 @@ public final class Colorizer {
         }
 
         try {
-            Integer.parseInt(hex.substring(1), 16);
+            Integer.parseInt(hex.substring(1), HEXADECIMAL);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Could not parse hex: " + hex, e);
         }

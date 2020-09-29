8db08c2011d23a111a568baa81cea54ec500211a
Siroshun09 <siro.shun8@gmail.com>
2020-09-29 22:51:13 +0900

    [Change] BukkitConfig をインスタンスとして作成し、そこから値を取得する

--- src/main/java/net/okocraft/scoreboard/config/Configuration.java
+++ src/main/java/net/okocraft/scoreboard/config/Configuration.java
@@ -4,21 +4,23 @@ import com.github.siroshun09.configapi.bukkit.BukkitConfig;
 import net.okocraft.scoreboard.ScoreboardPlugin;
 import org.jetbrains.annotations.NotNull;
 
-public class Configuration extends BukkitConfig {
+public class Configuration {
+
+    private final BukkitConfig config;
 
     public Configuration(@NotNull ScoreboardPlugin plugin) {
-        super(plugin, "config.yml", true);
+        config = new BukkitConfig(plugin, "config.yml", true);
     }
 
     public boolean isUsingProtocolLib() {
-        return getBoolean("use-ProtocolLib", true);
+        return config.getBoolean("use-ProtocolLib", true);
     }
 
     public int getLengthLimit() {
-        return Math.max(getInt("board.limit", 32), 1);
+        return Math.max(config.getInt("board.limit", 32), 1);
     }
 
     public int getThreads() {
-        return Math.max(getInt("board.threads", 5), 1);
+        return Math.max(config.getInt("board.threads", 5), 1);
     }
 }

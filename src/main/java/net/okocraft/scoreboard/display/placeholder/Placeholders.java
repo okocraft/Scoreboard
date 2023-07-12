979272134982b9deb7aa0eff7956f2d5646e4b23
Siroshun09 <siro.shun8@gmail.com>
2023-07-13 04:05:19 +0900

    feat: add ram placeholders

--- src/main/java/net/okocraft/scoreboard/display/placeholder/Placeholders.java
+++ src/main/java/net/okocraft/scoreboard/display/placeholder/Placeholders.java
@@ -58,6 +58,10 @@ public final class Placeholders {
         return switch (placeholder) {
             case "%server_tps%" -> BigDecimal.valueOf(Bukkit.getTPS()[0]).setScale(2, RoundingMode.HALF_UP).toPlainString();
             case "%server_online%" -> Integer.toString(Bukkit.getOnlinePlayers().size());
+            case "%server_ram_used%" -> toMB(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+            case "%server_ram_free%" -> toMB(Runtime.getRuntime().freeMemory());
+            case "%server_ram_total%" -> toMB(Runtime.getRuntime().totalMemory());
+            case "%server_ram_max%" -> toMB(Runtime.getRuntime().maxMemory());
             case "%player_name%" -> player.getName();
             case "%player_displayname%" -> LegacyComponentSerializer.legacyAmpersand().serialize(player.displayName());
             case "%player_world%" -> player.getWorld().getName();
@@ -68,6 +72,10 @@ public final class Placeholders {
             default -> placeholder;
         };
         //@formatter:on
+    }
+
+    private static @NotNull String toMB(long bytes) {
+        return Long.toString(bytes >> 20); // bytes / 1024 / 1024 (MB)
     }
 
     private Placeholders() {

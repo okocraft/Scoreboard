e1506cb8554b9e1d6d5f69a28bdce59faa7a841f
Siroshun09 <siro.shun8@gmail.com>
2024-05-15 10:49:02 +0900

    clean: remove old JavaScheduler

--- src/main/java/net/okocraft/scoreboard/util/scheduler/Scheduler.java
+++ src/main/java/net/okocraft/scoreboard/util/scheduler/Scheduler.java
@@ -2,18 +2,12 @@ package net.okocraft.scoreboard.util.scheduler;
 
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import net.okocraft.scoreboard.task.UpdateTask;
-import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 public interface Scheduler {
 
     static @NotNull Scheduler create() {
-        try {
-            Bukkit.class.getDeclaredMethod("getAsyncScheduler");
-            return new PaperModernScheduler();
-        } catch (NoSuchMethodException e) {
-            return new JavaScheduler();
-        }
+        return new PaperModernScheduler();
     }
 
     @CanIgnoreReturnValue
